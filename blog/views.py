ae050cdd39323763defd13eaa8470d0ea1c27c8f
Christof Franke <christof@myway.de>
2017-04-04 15:56:21 +0200

    captcha audio in popover: cleanup

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.forms import ModelForm
-from django.forms import HiddenInput, Textarea, TextInput
+from django.forms import HiddenInput
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.core.urlresolvers import reverse
 
 from contents.views import render_content_to_string
 
-from captcha.fields import CaptchaField, CaptchaTextInput
+from captcha.fields import CaptchaField
 from captcha.models import CaptchaStore
 from captcha.helpers import captcha_image_url
 
@@ -15,20 +15,20 @@ from .models import BlogEntry
 from .models import RichTextContent, ImageContent
 from .models import Comment
 
+
 class CommentForm(ModelForm):
-
-    # captcha = CaptchaField()
-
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'website', 'comment','parent']
+        fields = ['name', 'email', 'website', 'comment', 'parent']
         widgets = {'parent': HiddenInput(), }
+
 
 class CaptchaCommentForm(ModelForm):
     captcha = CaptchaField(required=True)
+
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'website', 'comment','parent']
+        fields = ['name', 'email', 'website', 'comment', 'parent']
         widgets = {
             'name': HiddenInput(),
             'email': HiddenInput(),
@@ -46,31 +46,34 @@ def comment_form_check(request):
             if form_captcha.is_valid():
                 form.save()
             else:
-                print(form_captcha.errors)
                 key = CaptchaStore.generate_key()
                 url = captcha_image_url(key)
                 audio_url = reverse('captcha-audio', args=[key])
                 # need no exact error message but captcha reload.
                 # use data structure same as form.erros because javascript
                 # expecting it... (? dirty or not ?)
-                new_captcha = {'captcha_1': { 'url': url,
-                                             'audio_url': audio_url,
-                                             'key': key,
-                                             'errors':
-                                             form_captcha.errors,
-                                            }
-                              }
+                new_captcha = {
+                    'captcha_1': {
+                        'url': url,
+                        'audio_url': audio_url,
+                        'key': key,
+                        'errors':
+                        form_captcha.errors,
+                    }
+                }
                 return JsonResponse(new_captcha)
         return JsonResponse(form.errors)
     else:
         return HttpResponseNotAllowed(('POST',))
 
+
 def entry_detail(request, slug):
     entry = get_object_or_404(BlogEntry, slug=slug)
-    contents = render_content_to_string(request, entry, [RichTextContent, ImageContent])
+    contents = render_content_to_string(request, entry,
+                                        [RichTextContent, ImageContent])
     comments = Comment.objects.get_comments(entry)
-    comment_form = CommentForm(initial={'parent':entry, })
-    comment_form_captcha = CaptchaCommentForm(initial={'parent':entry, })
+    comment_form = CommentForm(initial={'parent': entry, })
+    comment_form_captcha = CaptchaCommentForm(initial={'parent': entry, })
 
     return render(request, 'blog/blogentry_detail.html', {
         'object': entry,
--- core/static/core/js/forms.js
+++ core/static/core/js/forms.js
@@ -14,122 +14,119 @@
  *                                            bootstrap warnings.
  **/
 function setUpAjaxForm(form_selector, url, options){
-  var AjaxForm = {
-    selector: form_selector,
-    url: url,
-    on_typing: true,
-    stop_typing_delay: 1100,
-
-    /**
-     * adds/ removes errors to html error div.
-     * @param {DOM el} input_element ie: $('input#id_email').
-     * @param {json} json_response ajax response with field errors.
-     */
-    onFieldError: function(element_name, json_response){
-      var error = json_response[element_name];
-      if(error){
-        $(this.selector+" #"+element_name+"_help").hide();
-        $(this.selector+" #"+element_name+"_errors").html(error);
-        $(this.selector+" #id_"+element_name).parent().addClass("has-error");;
-        $(this.selector+" #id_"+element_name).next().addClass("glyphicon-remove");
-      }else{
-        $(this.selector+" #"+element_name+"_help").show();
-        $(this.selector+" #"+element_name+"_errors").html("");
-        $(this.selector+" #id_"+element_name).parent().removeClass("has-error");
-        $(this.selector+" #id_"+element_name).parent().addClass("has-success");
-        $(this.selector+" #id_"+element_name).next().removeClass("glyphicon-remove");
-        $(this.selector+" #id_"+element_name).next().addClass("glyphicon-ok");
-      }
-    },
-    getFormData: function(){
-      var data = { };
-      $(this.selector+" :input").each(function(){
-        if($(this).attr("type") != "submit" ){
-          data[$(this).attr('name')] = $(this).val();
-        }
-      });
-      return data
-    },
-    onSuccess: function(){
-    },
-    /**
-     * check a singel input field.
-     * @input_element (dom element) ie: $('input#id_website')
-     */
-    validateSingleInput:  function(input_element){
-      var data = {[input_element.name]: input_element.value,
-        csrfmiddlewaretoken: getCSRF(), };
-      var self=this;
-      return $.post({
-        data: data,
-        url: this.url,
-        success: function (json_respons){
-          self.onFieldError(input_element.name, json_respons);
-        },
-      });
-    },
-    /**
-     * check the whole form (all input fields).
-     */
-    validateAllInputs: function(){
-      var self=this;
-      return $.post({
-        data: this.getFormData(),
-        url: this.url,
-        success: function (json_respons){
-          var err = 0;
-          for(var element_name in json_respons){
-            self.onFieldError(element_name, json_respons);
-            err++;
-          }
-          if(err == 0) {
-            self.onSuccess();
-          }
-        },
-      });
-    },
-
-    setUpTypingValidation: function(){
-      var typingTimer; //timer identifier
-      var self=this;
-      var func_check = function(e){
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(function(){
-          return self.validateSingleInput(e.target);
-        }, self.stop_typing_delay);
-
-      };
-      var self = this;
-      $(this.selector+" :input").each(function(){
-        if($(this).attr("type") != "submit" && $(this).attr("type") != "hidden"){
-          $(this).keyup(func_check);
-          $(this).keydown(function(){clearTimeout(typingTimer)});
-          $(this).focusout(function(e){return self.validateSingleInput(e.target);});
-        }
-      });
-    },
-
-    setUpFormValidation: function(){
-      var self = this;
-      $(this.selector+" :submit").click(function(e){
-        self.validateAllInputs();
-        return false; // avoid to execute the actual form submit
-      }); //click
-    },
-
-    setItUp: function(){
-      if(this.on_typing) {
-        this.setUpTypingValidation();
-      }
-      this.setUpFormValidation();
-    },
-
-  }; // AjaxForm
-	// only one argument, options
-	if(typeof form_selector === 'object' && typeof url === 'undefined' && typeof options === 'undefined'){
-		options = form_selector;
+	var AjaxForm = {
+		selector: form_selector,
+		url: url,
+		on_typing: true,
+		stop_typing_delay: 1100,
+		/**
+		 * adds/ removes errors to html error div.
+		 * @param {DOM el} input_element ie: $('input#id_email').
+		 * @param {json} json_response ajax response with field errors.
+		 */
+		onFieldError: function(element_name, json_response){
+			var error = json_response[element_name];
+			if(error){
+				$(this.selector+" #"+element_name+"_help").hide();
+				$(this.selector+" #"+element_name+"_errors").html(error);
+				$(this.selector+" #id_"+element_name).parent().addClass("has-error");;
+				$(this.selector+" #id_"+element_name).next().addClass("glyphicon-remove");
+			}else{
+				$(this.selector+" #"+element_name+"_help").show();
+				$(this.selector+" #"+element_name+"_errors").html("");
+				$(this.selector+" #id_"+element_name).parent().removeClass("has-error");
+				$(this.selector+" #id_"+element_name).parent().addClass("has-success");
+				$(this.selector+" #id_"+element_name).next().removeClass("glyphicon-remove");
+				$(this.selector+" #id_"+element_name).next().addClass("glyphicon-ok");
+			}
+		},
+		getFormData: function(){
+			var data = { };
+			$(this.selector+" :input").each(function(){
+				if($(this).attr("type") != "submit" ){
+					data[$(this).attr('name')] = $(this).val();
+				}
+			});
+			return data
+		},
+		onSuccess: function(){
+		},
+		/**
+		 * check a singel input field.
+		 * @input_element (dom element) ie: $('input#id_website')
+		 */
+		validateSingleInput:  function(input_element){
+			var data = {[input_element.name]: input_element.value,
+				csrfmiddlewaretoken: getCSRF(), };
+			var self=this;
+			return $.post({
+				data: data,
+				url: this.url,
+				success: function (json_respons){
+					self.onFieldError(input_element.name, json_respons);
+				},
+			});
+		},
+		/**
+		 * check the whole form (all input fields).
+		 */
+		validateAllInputs: function(){
+			var self=this;
+			return $.post({
+				data: this.getFormData(),
+				url: this.url,
+				success: function (json_respons){
+					var err = 0;
+					for(var element_name in json_respons){
+						self.onFieldError(element_name, json_respons);
+						err++;
+					}
+					if(err == 0) {
+						self.onSuccess();
+					}
+				},
+			});
+		},
+		setUpTypingValidation: function(){
+			var typingTimer; //timer identifier
+			var self=this;
+			var func_check = function(e){
+				clearTimeout(typingTimer);
+				typingTimer = setTimeout(function(){
+					return self.validateSingleInput(e.target);
+				}, self.stop_typing_delay);
+			};
+			var self = this;
+			$(this.selector+" :input").each(function(){
+				if($(this).attr("type") != "submit" && $(this).attr("type") != "hidden"){
+					$(this).keyup(func_check);
+					$(this).keydown(function(){clearTimeout(typingTimer)});
+					$(this).focusout(function(e){
+						return self.validateSingleInput(e.target);
+					});
+				}
+			});
+		},
+		setUpFormValidation: function(){
+			var self = this;
+			$(this.selector+" :submit").click(function(e){
+				self.validateAllInputs();
+				return false; // avoid to execute the actual form submit
+			}); //click
+		},
+		setItUp: function(){
+			if(this.on_typing) {
+				this.setUpTypingValidation();
+			}
+			this.setUpFormValidation();
+		},
+	}; // AjaxForm
+	// only one argument -> use it as options
+	if(typeof form_selector === 'object' && typeof url === 'undefined'
+		&& typeof options === 'undefined'){
+			options = form_selector;
 	}
-  $.extend(AjaxForm, options);
-  AjaxForm.setItUp();
+	$.extend(AjaxForm, options);
+	AjaxForm.setItUp();
 	return AjaxForm
-}
+} // setUpAjaxForm
