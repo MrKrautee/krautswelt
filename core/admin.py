4d7f1afdc6cf8c09cab14bc63869139cc304d43c
Christof Franke <christof@myway.de>
2017-12-25 07:48:32 +0100

    subnavigation, UserAdmin added, Admin link in base.html

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as OldUserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(OldUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('date_joined',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
---
+++ core/pages/templatetags/page.py
@@ -0,0 +1,30 @@
+from django import template
+from django.utils.html import mark_safe
+
+register = template.Library()
+
+@register.inclusion_tag('pages/sub_pages_navigation.html', takes_context=False)
+def full_sub_navigation(current_page):
+    root = current_page.get_root()
+    if not root.get_children():
+        return dict(sub_pages = [], sub_pages_html = '')
+    full_tree = root.get_descendants(include_self=True)
+    html = "<ul>"
+    li_link = "<li><a href='%s'>%s</a></li>"
+    bold = "<b>%s</b>"
+    level = 0
+    for page in full_tree:
+        if level < page.level:
+            html += "<ul>"
+        elif level > page.level:
+            html += "</ul>"
+        if page == current_page:
+            title = bold % page.title
+        else:
+            title = page.title
+        html += li_link % (page.get_absolute_url(), title)
+        level = page.level
+    html += "</ul>"
+    return dict(sub_pages_html = mark_safe(html), sub_pages = full_tree)
+
+
